c089c1966e871debae0783569958a27bdcedf2df
tiemonl <tiemon.liam@gmail.com>
2018-09-16 18:22:50 -0400

    checking OS version to get token path

--- LeaderBot/GetKey.cs
+++ LeaderBot/GetKey.cs
@@ -1,11 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace LeaderBot {
 	public class GetKey {
 
 		public static string getKey(string bot) {
-			string keyPath = Path.Combine("Tokens", $"{bot}.token");
+			string keyPath = null;
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+				keyPath = Path.Combine("..", "..", "..", "Tokens", $"{bot}.token");
+			else {
+				keyPath = Path.Combine("Tokens", $"{bot}.token");
+			}
 			if (File.Exists(keyPath)) {
 				using (StreamReader sr = new StreamReader(keyPath, Encoding.UTF8)) {
 					string key = sr.ReadToEnd();
