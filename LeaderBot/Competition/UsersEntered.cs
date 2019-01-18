ee09f60db98082daee02f7cc849caade362ca416
tiemonl <tiemon.liam@gmail.com>
2019-01-17 19:04:12 -0500

    competition for shad

---
+++ LeaderBot/Competition/UsersEntered.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LeaderBot.Competition
+{
+    public class UsersEntered
+    {
+        public long _id { get; set; }
+        public string name { get; set; }
+        public int credits { get; set; }
+        public UsersEntered()
+        {
+        }
+    }
+}
